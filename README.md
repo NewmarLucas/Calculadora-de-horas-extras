1722dfe0d500efb651f5078e64e74d69737d7312
unknown <newmarlucas@gmail.com>
2021-04-28 19:57:19 -0400

    arrumando a logica

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Calculadora-de-horas-extras
 
 Um projeto simples feito com JavaScript, HTML e CSS. Feito para calcular as horas trabalhadas por dia para facilitar quem precisa calcular as horas extras de cadernos de ponto.
+
+Projeto baseado [desse site](http://www.sistemaseweb.com.br/computacaoemfoco/CalculadorHoras.html), que foi feito usando JQuery, e eu reimplementei ele usando JavaScript puro.
--- index.js
+++ index.js
@@ -6,55 +6,34 @@ function calculaHoras() {
     var horaVoltaAlmoco = document.getElementById('horaVoltaAlmoco').value;
     var horaSaida = document.getElementById('horaSaida').value;
 
-    // Limpa os campos calculados
     document.getElementById('diferencaPrimeiroTurno').innerHTML = '';
-    document.getElementById('tempoRestante').innerHTML = '';
+    document.getElementById('diferencaSegundoTurno').innerHTML = '';
     document.getElementById('saldoHoras').innerHTML = '';
 
+    var primeiroTurno = diferencaHoras(horaEntrada, horaIdaAlmoco);
+    document.getElementById('diferencaPrimeiroTurno').innerHTML = primeiroTurno;
 
-    // Calcula o intervalo entre a entrada e o almoco
-    var diffEntradaAlmoco = diferencaHoras(horaEntrada, horaIdaAlmoco);
-    document.getElementById('diferencaPrimeiroTurno').innerHTML = diffEntradaAlmoco;
+    var segundoTurno = diferencaHoras(horaVoltaAlmoco, horaSaida);
+    document.getElementById('diferencaSegundoTurno').innerHTML = segundoTurno;
 
+    var somaTurnos = somaHora(primeiroTurno, segundoTurno);
 
-    var tempoRestante = diferencaHoras(diffEntradaAlmoco, totalHorasDia);
-    document.getElementById('tempoRestante').innerHTML = tempoRestante;
-
-    var horaSaidaCerta = somaHora(horaVoltaAlmoco, tempoRestante);
-		document.getElementById('horaSaidaMinima').innerHTML = horaSaidaCerta;
-
-    // Caso a pessoa vá embora antes ou depois do horário ideal, calcula
-    // o tempo q mais ou a menos que ela trabalhou do total que ela devia
-    // trabalhar
-    var saldoHoras = diferencaHoras(horaSaidaCerta, horaSaida);
+    var saldoHoras = diferencaHoras(totalHorasDia, somaTurnos);
     document.getElementById('saldoHoras').innerHTML = saldoHoras;
 
-    // Se ela ficou tempo a mais, o saldo é positivo; se a pessoa
-    // saiu mais cedo do que devia, o saldo é negativo
-    if (isHoraInicialMenorHoraFinal(horaSaida, horaSaidaCerta)) {
-        document.getElementById('saldoHoras').innerHTML = "-" + document.getElementById('saldoHoras');
+    if (totalHorasDia < somaTurnos) {
+        document.getElementById('saldoHoras').innerHTML = "-" + saldoHoras;
     }
+
 }
 
-
-/**
-* Retona a diferença entre duas horas.
-* Exemplo: 14:35 a 17:21 = 02:46
-*/
 function diferencaHoras(horaInicial, horaFinal) {
-
-    // Tratamento se a hora inicial é menor que a final	
-    if (!isHoraInicialMenorHoraFinal(horaInicial, horaFinal)) {
-        aux = horaFinal;
-        horaFinal = horaInicial;
-        horaInicial = aux;
-    }
 
     var hIni = horaInicial.split(':');
     var hFim = horaFinal.split(':');
 
-    horasTotal = parseInt(hFim[0], 10) - parseInt(hIni[0], 10);
-    minutosTotal = parseInt(hFim[1], 10) - parseInt(hIni[1], 10);
+    var horasTotal = parseInt(hFim[0], 10) - parseInt(hIni[0], 10);
+    var minutosTotal = parseInt(hFim[1], 10) - parseInt(hIni[1], 10);
 
     if (minutosTotal < 0) {
         minutosTotal += 60;
@@ -65,52 +44,23 @@ function diferencaHoras(horaInicial, horaFinal) {
     return horaFinal;
 }
 
-/**
-* Soma duas horas.
-* Exemplo:  12:35 + 07:20 = 19:55.
-*/
 function somaHora(horaInicio, horaSomada) {
 
-    horaIni = horaInicio.split(':');
-    horaSom = horaSomada.split(':');
+    var horaIni = horaInicio.split(':');
+    var horaSom = horaSomada.split(':');
 
-    horasTotal = parseInt(horaIni[0], 10) + parseInt(horaSom[0], 10);
-    minutosTotal = parseInt(horaIni[1], 10) + parseInt(horaSom[1], 10);
+    var horasTotal = parseInt(horaIni[0], 10) + parseInt(horaSom[0], 10);
+    var minutosTotal = parseInt(horaIni[1], 10) + parseInt(horaSom[1], 10);
 
     if (minutosTotal >= 60) {
         minutosTotal -= 60;
         horasTotal += 1;
     }
 
-    horaFinal = completaZeroEsquerda(horasTotal) + ":" + completaZeroEsquerda(minutosTotal);
+    var horaFinal = completaZeroEsquerda(horasTotal) + ":" + completaZeroEsquerda(minutosTotal);
     return horaFinal;
 }
 
-/**
- * Verifica se a hora inicial é menor que a final.
- */
-function isHoraInicialMenorHoraFinal(horaInicial, horaFinal) {
-    horaIni = horaInicial.split(':');
-    horaFim = horaFinal.split(':');
-
-    // Verifica as horas. Se forem diferentes, é só ver se a inicial 
-    // é menor que a final.
-    hIni = parseInt(horaIni[0], 10);
-    hFim = parseInt(horaFim[0], 10);
-    if (hIni != hFim)
-        return hIni < hFim;
-
-    // Se as horas são iguais, verifica os minutos então.
-    mIni = parseInt(horaIni[1], 10);
-    mFim = parseInt(horaFim[1], 10);
-    if (mIni != mFim)
-        return mIni < mFim;
-}
-
-/**
- * Completa um número menor que dez com um zero à esquerda.
- * Usado aqui para formatar as horas... Exemplo: 3:10 -> 03:10 , 10:5 -> 10:05
- */
 function completaZeroEsquerda(numero) {
     return (numero < 10 ? "0" + numero : numero);
 }
